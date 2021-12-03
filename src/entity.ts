733c2b2d6d136ecfb11932c84c3594bd137b8adf
Tom Gallacher <tgallacher@users.noreply.github.com>
2021-12-03 14:03:56 +0000

    fix: entity _data possibly undefined

--- src/entity.ts
+++ src/entity.ts
@@ -11,28 +11,37 @@ export interface IEntity {
   equals(object?: IEntity): boolean;
 }
 
+interface defaultConstructorData {
+  tenantId: string;
+  id: string;
+}
+
 /**
  * An object whose definition is based on `identity` over just its attributes.
  *
  * Also known as `Reference Objects`.
  */
 export abstract class Entity<
-  T extends Partial<{ id: string; tenantId?: string }> = {
-    id?: string;
-    tenantId?: string;
-  },
+  T extends Partial<defaultConstructorData> = Partial<defaultConstructorData>,
 > implements IEntity
 {
   private readonly _id: string;
   private readonly _tenantId: string | null;
 
-  protected readonly _data: T | undefined;
+  protected readonly _data: Omit<T, 'id' | 'tenantId'>;
 
   // Make `id` optional accounting for re-consituting objects from persistence
   constructor(data?: T) {
-    this._data = data;
     this._tenantId = data?.tenantId ?? null;
     this._id = data?.id ?? uuid();
+
+    // remove captured fields, if they exist
+    delete data?.id;
+    delete data?.tenantId;
+
+    // @ts-expect-error
+    // fixme: check typing
+    this._data = data ?? {};
   }
 
   public get id() {
